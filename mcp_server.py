81b54a9013ed3b74a9bec56229a981591146651a
nantaporndo <nantapornd56@gmail.com>
2026-05-24 17:23:32 +0700

    add mcp structure

--- mcp_server.py
+++ mcp_server.py
@@ -40,8 +40,18 @@ def edit_document(doc_id:str=Field(description="Id of the document to edit."),
     docs[doc_id] = docs[doc_id].replace(old_str,new_str)
 
 
-# TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
+@mcp.resource("docs://documents")
+def list_documents() -> list[str]:
+    return list(docs.keys())
+
+
+@mcp.resource("docs://{doc_id}")
+def get_document(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document '{doc_id}' not found.")
+    return docs[doc_id]
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
+if __name__ == "__main__":
+    mcp.run()
